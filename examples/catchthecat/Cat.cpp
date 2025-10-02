c7b5105358018796c52c08337c49534469acb3de
BrandonCherry166 <brandon.cherry@mymail.champlain.edu>
2025-10-02 10:12:07 -0400

    Setup for Catch the Cat, setting up 2D vectors and queue for BFS implementation.

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -1,24 +1,24 @@
 #include "Cat.h"
 #include "World.h"
 #include <stdexcept>
+#include <vector>
+#include <queue>
 
 Point2D Cat::Move(World* world) {
-  auto rand = Random::Range(0, 5);
-  auto pos = world->getCat();
-  switch (rand) {
-    case 0:
-      return World::NE(pos);
-    case 1:
-      return World::NW(pos);
-    case 2:
-      return World::E(pos);
-    case 3:
-      return World::W(pos);
-    case 4:
-      return World::SW(pos);
-    case 5:
-      return World::SE(pos);
-    default:
-      throw "random out of range";
-  }
+  int sideLength = world->getWorldSideSize();
+  Point2D pos = world->getCat();
+  int dr_even[6] = {-1, -1, 0, 0, 1, 1};
+  int dc_even[6] = {0, 1, -1, 1, 0, 1};
+  int dr_odd[6] = {-1, -1, 0, 0, 1, 1};
+  int dc_odd[6] = {-1, 0, -1, 1, -1, 0};
+
+  std::vector<std::vector<bool>> visited (sideLength, std::vector<bool>(sideLength, false)); //2D Vector of Visited Cells
+  std::vector<std::vector<Point2D>> parent (sideLength, std::vector<Point2D>(sideLength, Point2D(-1, -1)));
+
+  std::queue<Point2D> q;
+
+  q.push(pos); //Add starting position
+  visited[pos.y][pos.x] = true;
+
+
 }
