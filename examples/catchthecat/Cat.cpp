ce94d739977ac418d6ac1b1dbe9d67301aa67e77
BrandonCherry166 <brandon.cherry@mymail.champlain.edu>
2025-10-02 12:25:44 -0400

    Lots of rewrites, no progress

--- examples/catchthecat/Cat.cpp
+++ examples/catchthecat/Cat.cpp
@@ -36,6 +36,9 @@ Point2D GetParent(std::unordered_map<int, std::unordered_map<int, Point2D>>& par
 Point2D Cat::Move(World* world) {
   Point2D start = world -> getCat();
   int sideLength = world->getWorldSideSize();
+  int halfLength = sideLength / 2;
+  int minBound = -halfLength;
+  int maxBound = (sideLength % 2 == 0) ? halfLength - 1 : halfLength;
   std::unordered_map<int,std::unordered_map<int, bool>> visited;
   std::unordered_map<int, std::unordered_map<int, Point2D>> parent;
 
@@ -48,13 +51,18 @@ Point2D Cat::Move(World* world) {
     Point2D current = q.front();
     q.pop();
 
-    if (current.x == 0 || current.y == 0 || current.x == sideLength -1 || current.y == sideLength - 1) { //Checking Boundaries
+    std::cout << current.x << ", " << current.y << std::endl;
+    if (current.x == minBound || current.y == minBound || current.x == maxBound || current.y == maxBound) { //Checking Boundaries
       std::vector<Point2D> path;
       for (Point2D v = current; HasParent(parent, v); v = GetParent(parent, v)) {
         path.push_back(v);
       }
       path.push_back(start);
       std::reverse(path.begin(), path.end());
+      std::cout << "Path to Boundary: " << std::endl;
+      for (auto &p: path) {
+        std::cout << p.x << ", " << p.y << std::endl;
+      }
 
       if (path.size() >= 2) {
         return path[1]; //Take the first step
@@ -67,10 +75,10 @@ Point2D Cat::Move(World* world) {
       {
       world->E(current),
       world->W(current),
+      world->NE(current),
+      world->NW(current),
       world->SE(current),
-      world->SW(current),
-      world->NW(current),
-      world->NW(current),
+      world->SW(current)
       };
     for (auto& next : neighbors) {
       if (!IsVisited(visited, next) && world->catCanMoveToPosition(next)) {
